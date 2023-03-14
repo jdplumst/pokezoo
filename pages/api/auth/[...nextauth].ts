049e179279003dacbf145e95699a84c0985af5ee
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-14 04:49:35 -0400

    Install Prisma Adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import prisma from "@/prisma/script";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
