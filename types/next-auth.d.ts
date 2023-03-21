e139c5e0eb0dff00699b2778445fed10336b67a6
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-21 13:32:22 -0400

    Fix User type and make it easier to modify

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,21 +3,11 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   // Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
   interface Session {
-    user: {
-      id: number | string;
-      name: string | null;
-      email: string | null;
-      emailVerified: Date | null;
-      image: string;
-
-      // Custom fields
-      totalYield: number;
-      dollars: number;
-    };
+    user: User;
   }
 
   interface User {
-    id: number | string;
+    id: string;
     name: string | null;
     email: string | null;
     emailVerified: Date | null;
