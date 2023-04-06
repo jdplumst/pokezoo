a193a04275cee9c932d5e65c050286ddfd4e96fe
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-06 16:42:09 -0400

    Add link to Game page on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light relative flex w-full flex-wrap items-center justify-around py-4 font-bold shadow-lg">
       <div className="w-24"></div>
-      <h1 className="text-4xl">PokéZoo</h1>
+      <Link href="/game">
+        <h1 className="text-4xl">PokéZoo</h1>
+      </Link>
       <button
         onClick={() => signOut()}
         className="w-24 rounded-lg border-2 border-black bg-slate-400 p-2 font-bold hover:bg-slate-500">
