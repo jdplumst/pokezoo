b0142cff81c73cd6cd0e96759a58db0cf3c814f4
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-07 20:50:49 -0400

    Add Ball component

---
+++ components/Ball.tsx
@@ -0,0 +1,35 @@
+import { Balls } from "@prisma/client";
+import Image from "next/image";
+import Tooltip from "./Tooltip";
+
+interface IBall {
+  ball: Balls;
+  purchaseBall: (ball: Balls) => void;
+}
+
+export default function Ball({ ball, purchaseBall }: IBall) {
+  return (
+    <Tooltip ball={ball}>
+      <div className="h-72 w-72 border-2 border-black bg-emerald-500 p-2">
+        <div className="flex h-full flex-col items-center justify-around">
+          <Image
+            src={ball.img}
+            alt={ball.name}
+            width={112}
+            height={112}
+            className="pixelated"
+          />
+          <p className="text-center text-3xl font-bold">{ball.name} Ball</p>
+          <p className="text-center text-2xl font-bold">
+            P{ball.cost.toLocaleString()}
+          </p>
+          <button
+            onClick={() => purchaseBall(ball)}
+            className="w-24 rounded-lg border-2 border-black bg-indigo-500 p-2 font-bold hover:bg-indigo-600">
+            Buy
+          </button>
+        </div>
+      </div>
+    </Tooltip>
+  );
+}
