8876f6ef1f2cff5be5a713a061187f54ca73958f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-07 20:46:05 -0400

    Modify Tooltip to use new Balls type

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,9 +1,8 @@
+import { Balls } from "@prisma/client";
 import { ReactNode, useState } from "react";
 
-export type Ball = "Poke" | "Great" | "Ultra" | "Master";
-
 interface ITooltip {
-  ball: Ball;
+  ball: Balls;
   children: ReactNode;
 }
 
@@ -17,74 +16,25 @@ export default function Tooltip({ ball, children }: ITooltip) {
       className="tooltip-container">
       {visible && (
         <div className="tooltip z-10 min-w-[200px] bg-blue-700 p-4 text-black">
-          {ball === "Poke" && (
-            <div>
-              Opening this ball gives:
-              <p>
-                80% Chance <span className="text-white">Common</span>
-              </p>
-              <p>
-                20% Chance <span className="text-orange-500">Rare</span>
-              </p>
-              <p>
-                0% Chance <span className="text-purple-500">Epic</span>
-              </p>
-              <p>
-                0% Chance <span className="text-yellow-500">Legendary</span>
-              </p>
-            </div>
-          )}
-          {ball === "Great" && (
-            <div>
-              Opening this ball gives:
-              <p>
-                65% Chance <span className="text-white">Common</span>
-              </p>
-              <p>
-                30% Chance <span className="text-orange-500">Rare</span>
-              </p>
-              <p>
-                5% Chance <span className="text-purple-500">Epic</span>
-              </p>
-              <p>
-                0% Chance <span className="text-yellow-500">Legendary</span>
-              </p>
-            </div>
-          )}
-          {ball === "Ultra" && (
-            <div>
-              Opening this ball gives:
-              <p>
-                25% Chance <span className="text-white">Common</span>
-              </p>
-              <p>
-                50% Chance <span className="text-orange-500">Rare</span>
-              </p>
-              <p>
-                23% Chance <span className="text-purple-500">Epic</span>
-              </p>
-              <p>
-                2% Chance <span className="text-yellow-500">Legendary</span>
-              </p>
-            </div>
-          )}
-          {ball === "Master" && (
-            <div>
-              Opening this ball gives:
-              <p>
-                15% Chance <span className="text-white">Common</span>
-              </p>
-              <p>
-                25% Chance <span className="text-orange-500">Rare</span>
-              </p>
-              <p>
-                50% Chance <span className="text-purple-500">Epic</span>
-              </p>
-              <p>
-                10% Chance <span className="text-yellow-500">Legendary</span>
-              </p>
-            </div>
-          )}
+          <div>
+            Opening this ball gives:
+            <p>
+              {ball.commonChance}% Chance{" "}
+              <span className="text-white">Common</span>
+            </p>
+            <p>
+              {ball.rareChance}% Chance{" "}
+              <span className="text-orange-500">Rare</span>
+            </p>
+            <p>
+              {ball.epicChance}% Chance{" "}
+              <span className="text-purple-500">Epic</span>
+            </p>
+            <p>
+              {ball.legendaryChance}% Chance{" "}
+              <span className="text-yellow-500">Legendary</span>
+            </p>
+          </div>
         </div>
       )}
       {children}
