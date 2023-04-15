f547a3b8c0f7facfcb1896e9a416591e36c645d4
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-15 15:41:04 -0400

    Highlight Sidebar buttons when hovering

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export default function Sidebar({ children, page }: ISidebar) {
           <div
             className={`${
               page === "Game" && `bg-white`
-            } mb-4 flex w-full items-center`}>
+            } mb-4 flex w-full items-center hover:bg-slate-300`}>
             <Image
               src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/master-ball.png"
               alt="game"
@@ -55,7 +55,7 @@ export default function Sidebar({ children, page }: ISidebar) {
           <div
             className={`${
               page === "Shop" && `bg-white`
-            } mb-4 flex w-full items-center`}>
+            } mb-4 flex w-full items-center hover:bg-slate-300`}>
             <Image
               src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/amulet-coin.png"
               alt="shop"
@@ -72,7 +72,7 @@ export default function Sidebar({ children, page }: ISidebar) {
           <div
             className={`${
               page === "PatchNotes" && `bg-white`
-            } mb-4 flex w-full items-center`}>
+            } mb-4 flex w-full items-center hover:bg-slate-300`}>
             <Image
               src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/rule-book.png"
               alt="patch_notes"
@@ -87,7 +87,7 @@ export default function Sidebar({ children, page }: ISidebar) {
         </Link>
         <div className="mt-auto">
           <a href="https://pokeapi.co/" target="_blank">
-            <div className="flex items-center pb-4">
+            <div className="mb-2 flex items-center hover:bg-slate-300">
               <Image
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
                 alt="pokeapi"
@@ -101,7 +101,7 @@ export default function Sidebar({ children, page }: ISidebar) {
             </div>
           </a>
           <a href="https://github.com/jdplumst/pokezoo" target="_blank">
-            <div className="flex items-center pb-6">
+            <div className="mb-2 flex items-center py-2 hover:bg-slate-300">
               <GoMarkGithub
                 size={30}
                 className={`${open ? `ml-3` : `ml-auto mr-auto`} pixelated`}
@@ -118,7 +118,7 @@ export default function Sidebar({ children, page }: ISidebar) {
             onClick={() => signOut()}
             className={`${
               open ? `` : `ml-auto mr-auto`
-            } pixelated flex items-center`}>
+            } pixelated flex w-full items-center py-2 hover:bg-slate-300`}>
             <GoSignOut
               size={30}
               className={`${open ? `ml-3` : `ml-auto mr-auto`} pixelated mt-1`}
