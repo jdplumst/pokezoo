6ead86547928e9bb6f62a73d094bd49350a6047e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-23 08:52:18 -0400

    Add 1.14 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -16,6 +16,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <div className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">1.14 (April 23, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Net Ball.</p>
+            </div>
+            <hr className="border-black pb-4"></hr>
+            <div className="pb-4">
               <h3 className="p-4 text-4xl font-bold">1.13 (April 23, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">Added tutorial page.</p>
