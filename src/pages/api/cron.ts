435cb422aaf15eaca38932a46baf16c3dc740ec1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-22 22:37:10 -0400

    Throw error if cron fails

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -7,21 +7,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     !authorization ||
     authorization.split(" ")[1] !== process.env.CRON_TOKEN
   ) {
-    return res
-      .status(401)
-      .json({ error: "Not authorized to make this request." });
+    throw new Error("Not authorized to make this request.");
   }
   switch (req.method) {
     case "POST":
       try {
         await client.$executeRaw`UPDATE User SET balance = balance + totalYield, claimedDaily = false`;
-        return res
-          .status(200)
-          .json({
-            msg: "Successfully updated all user's dollars and reset daily claim"
-          });
+        return res.status(200).json({
+          msg: "Successfully updated all user's dollars and reset daily claim"
+        });
       } catch (error) {
-        return res.status(400).json({ error: error });
+        throw new Error(
+          "User's dollars and daily claim not updated successfully"
+        );
       }
   }
 };
