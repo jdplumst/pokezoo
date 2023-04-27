96fa2da9d25d7ce9e2c2183dbdd67394dee761f2
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-26 20:00:34 -0400

    Move styles folder into src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/src/styles/globals.css";
 import type { AppProps, AppType } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
