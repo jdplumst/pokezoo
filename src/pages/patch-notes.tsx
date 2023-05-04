3b5944a906e35afd5ccc1b64ced6b80e233c4a6b
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-04 14:58:21 -0400

    Add 2.04 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -16,6 +16,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <div className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">2.04 (May 4, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Dusk Ball.</p>
+            </div>
+            <hr className="border-black pb-4"></hr>
+            <div className="pb-4">
               <h3 className="p-4 text-4xl font-bold">2.03 (May 4, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="p-4">Added balance changes:</p>
