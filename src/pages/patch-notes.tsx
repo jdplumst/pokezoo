fb07292f4ff14a6053e123d699cfb4e5e012c272
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-10 20:59:58 -0400

    Add 2.09 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -16,6 +16,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">2.09 (May 10, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Updated colour scheme to be darker.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">2.08 (May 10, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="p-4">Added balance changes:</p>
