915b692d104a6033204c68e9a3df62c337d77db4
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-15 05:26:34 -0400

    Add script to add habitat data to Species table

---
+++ scripts/genTwoHabitat.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../src/server/db";
+
+// Script to populate Species collection
+const populateGenTwoHabitatDB = async () => {
+  for (let i = 11; i <= 251; i++) {
+    const speciesResponse = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${i}/`
+    );
+    const speciesData = await speciesResponse.json();
+
+    const species = await prisma.species.updateMany({
+      where: { pokedexNumber: i },
+      data: { habitat: speciesData.habitat.name }
+    });
+
+    console.log(species);
+  }
+};
+
+populateGenTwoHabitatDB();
