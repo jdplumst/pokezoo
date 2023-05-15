56f00e2790eeadb1e3c9ec29608b110985fc28b9
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-15 05:42:27 -0400

    Add 2.14 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -48,6 +48,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">2.14 (May 15, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added habitat information to cards.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">2.13 (May 14, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
