153e7a77fae84ba3f14e1b3bf4391facecca0444
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-18 14:53:56 -0400

    Add 2.16 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -48,6 +48,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">2.16 (May 18, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Dive Ball.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">2.15 (May 16, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
