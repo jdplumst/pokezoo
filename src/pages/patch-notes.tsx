9e8b970614c5175bbe29ad5178a9440dde36725e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-06-09 12:26:51 -0400

    Add 3.05 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -48,6 +48,14 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">3.05 (June 9, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">
+                Added habitat achievements for Kanto.
+              </p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">3.04 (June 8, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
