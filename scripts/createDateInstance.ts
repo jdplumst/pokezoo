6d8db38c746e81719245a2591c18822a873a0c28
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-06-09 18:18:09 -0400

    Add script for adding createDates to existing Instances

---
+++ scripts/createDateInstance.ts
@@ -0,0 +1,15 @@
+import { prisma } from "../src/server/db";
+
+const delay = (ms: number) => {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+const backfillCreateDateInstance = async () => {
+  const instances = await prisma.instance.findMany();
+  for (const i of instances) {
+    await delay(1000);
+    await prisma.instance.update({data: {createDate: new Date()}, where: {id: i.id}})
+  }
+}
+
+backfillCreateDateInstance();
