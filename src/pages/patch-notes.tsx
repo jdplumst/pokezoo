f65965b3504a3b905c6985b8648272f04a21dbe9
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-01 08:50:59 -0400

    Add 3.16 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -48,6 +48,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">3.16 (July 1, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added type filter to Pokédex page.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">3.15 (June 30, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
