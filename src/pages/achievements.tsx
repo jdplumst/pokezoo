e37880af34e8a13e55110f8bff0a8675708d9385
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-28 12:30:10 -0400

    Remove getServerSideProps from Achievements page

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -1,168 +1,45 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import { useEffect, useState } from "react";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { authOptions } from "./api/auth/[...nextauth]";
-import { getServerSession } from "next-auth";
-import { prisma } from "../server/db";
 import Loading from "../components/Loading";
 import ProgressBar from "../components/ProgressBar";
 import { FullAchievement } from "../components/ProgressBar";
 import Topbar from "../components/Topbar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { trpc } from "../utils/trpc";
+import LoadingSpinner from "../components/LoadingSpinner";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  if (!session || !session.user) {
-    return {
-      redirect: {
-        destination: "/"
-      }
-    };
-  }
+export default function Achievements() {
+  const router = useRouter();
 
-  const user = session?.user || null;
-  const instances = await prisma.instance.findMany({
-    where: { userId: user.id.toString() },
-    distinct: ["speciesId"],
-    include: {
-      species: {
-        select: {
-          rarity: true,
-          habitat: true,
-          typeOne: true,
-          typeTwo: true,
-          generation: true,
-          shiny: true
-        }
-      }
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/");
     }
   });
-  const species = await prisma.species.findMany();
-  const achievements = await prisma.achievement.findMany();
-  const userAchievements = await prisma.userAchievement.findMany({
-    where: { userId: user.id.toString() }
+
+  const { data: countData, isLoading: countLoading } =
+    trpc.instance.getCount.useQuery();
+
+  const { data: speciesData } = trpc.species.getSpecies.useQuery({
+    order: null
   });
 
-  // Calculate the percentage for each achievement
-  let fullAchievements: FullAchievement[] = [];
+  const { data: instanceData } = trpc.instance.getInstanceSpecies.useQuery();
 
-  for (let x = 0; x < achievements.length; x++) {
-    let max = 0;
-    let value = 0;
+  const { data: achievementData } = trpc.achievement.getAchievements.useQuery();
 
-    if (achievements[x].type === "All") {
-      max = species.filter(
-        (s) =>
-          s.generation === achievements[x].generation &&
-          s.shiny === achievements[x].shiny
-      ).length;
-      value = instances.filter(
-        (i) =>
-          i.species.generation === achievements[x].generation &&
-          i.species.shiny === achievements[x].shiny
-      ).length;
-    } else if (achievements[x].type === "Rarity") {
-      max = species.filter(
-        (s) =>
-          s.rarity === achievements[x].attribute &&
-          s.generation === achievements[x].generation &&
-          s.shiny === achievements[x].shiny
-      ).length;
-      value = instances.filter(
-        (i) =>
-          i.species.rarity === achievements[x].attribute &&
-          i.species.generation === achievements[x].generation &&
-          i.species.shiny === achievements[x].shiny
-      ).length;
-    } else if (achievements[x].type === "Habitat") {
-      max = species.filter(
-        (s) =>
-          s.habitat === achievements[x].attribute &&
-          s.generation === achievements[x].generation &&
-          s.shiny === achievements[x].shiny
-      ).length;
-      value = instances.filter(
-        (i) =>
-          i.species.habitat === achievements[x].attribute &&
-          i.species.generation === achievements[x].generation &&
-          i.species.shiny === achievements[x].shiny
-      ).length;
-    } else if (achievements[x].type === "Type") {
-      max = species.filter(
-        (s) =>
-          (s.typeOne === achievements[x].attribute ||
-            s.typeTwo === achievements[x].attribute) &&
-          s.generation === achievements[x].generation &&
-          s.shiny === achievements[x].shiny
-      ).length;
-      value = instances.filter(
-        (i) =>
-          (i.species.typeOne === achievements[x].attribute ||
-            i.species.typeTwo === achievements[x].attribute) &&
-          i.species.generation === achievements[x].generation &&
-          i.species.shiny === achievements[x].shiny
-      ).length;
-    }
+  const { data: userAchievementData } =
+    trpc.userAchievement.getUserAchievements.useQuery();
 
-    const low = 0.35 * max;
-    const high = 0.7 * max;
-    const percent = Math.round((value / max) * 100);
-    const achieved = userAchievements.some(
-      (u) => u.achievementId === achievements[x].id
-    );
-
-    fullAchievements.push({
-      achievement: achievements[x],
-      max: max,
-      value: value,
-      low: low,
-      high: high,
-      percent: percent,
-      achieved: achieved
-    });
-  }
-
-  fullAchievements.sort(function (a, b) {
-    if (a.achieved > b.achieved) return 1;
-    if (b.achieved > a.achieved) return -1;
-
-    if (a.percent >= b.percent) return -1;
-    if (b.percent < a.percent) return 1;
-    return 0;
-  });
-
-  // .sort((a, b) => (a.percent > b.percent ? 1 : -1))
-  // .sort((a, b) => (a.achieved > b.achieved ? 1 : -1));
-
-  let parsedInstances: typeof instances = JSON.parse(JSON.stringify(instances));
-  let parsedUserAchievements: typeof userAchievements = JSON.parse(
-    JSON.stringify(userAchievements)
-  );
-
-  return {
-    props: {
-      user,
-      instances: parsedInstances,
-      species,
-      fullAchievements,
-      userAchievements: parsedUserAchievements
-    }
-  };
-};
-
-export default function Achievements({
-  user,
-  instances,
-  species,
-  fullAchievements,
-  userAchievements
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [time, setTime] = useState<Time>("night");
   const [loading, setLoading] = useState(true);
-  const [totalYield, setTotalYield] = useState(user.totalYield);
+  const [totalYield, setTotalYield] = useState(0);
+  const [fullAchievements, setFullAchievements] = useState<FullAchievement[]>();
 
+  // Set time and total yield states
   useEffect(() => {
     const today = new Date();
     const hour = today.getHours();
@@ -171,14 +48,118 @@ export default function Achievements({
     } else {
       setTime("night");
     }
+
+    if (status !== "authenticated") return;
+    setTotalYield(session.user.totalYield);
+
     setLoading(false);
-  }, []);
+  }, [session]);
+
+  // Set full achievements to display progress bars
+  useEffect(() => {
+    if (speciesData && instanceData && achievementData && userAchievementData) {
+      let full: FullAchievement[] = [];
+
+      for (let x = 0; x < achievementData.achievements.length; x++) {
+        let max = 0;
+        let value = 0;
+
+        if (achievementData.achievements[x].type === "All") {
+          max = speciesData.species.filter(
+            (s) =>
+              s.generation === achievementData.achievements[x].generation &&
+              s.shiny === achievementData.achievements[x].shiny
+          ).length;
+          value = instanceData.instances.filter(
+            (i) =>
+              i.species.generation ===
+                achievementData.achievements[x].generation &&
+              i.species.shiny === achievementData.achievements[x].shiny
+          ).length;
+        } else if (achievementData.achievements[x].type === "Rarity") {
+          max = speciesData.species.filter(
+            (s) =>
+              s.rarity === achievementData.achievements[x].attribute &&
+              s.generation === achievementData.achievements[x].generation &&
+              s.shiny === achievementData.achievements[x].shiny
+          ).length;
+          value = instanceData.instances.filter(
+            (i) =>
+              i.species.rarity === achievementData.achievements[x].attribute &&
+              i.species.generation ===
+                achievementData.achievements[x].generation &&
+              i.species.shiny === achievementData.achievements[x].shiny
+          ).length;
+        } else if (achievementData.achievements[x].type === "Habitat") {
+          max = speciesData.species.filter(
+            (s) =>
+              s.habitat === achievementData.achievements[x].attribute &&
+              s.generation === achievementData.achievements[x].generation &&
+              s.shiny === achievementData.achievements[x].shiny
+          ).length;
+          value = instanceData.instances.filter(
+            (i) =>
+              i.species.habitat === achievementData.achievements[x].attribute &&
+              i.species.generation ===
+                achievementData.achievements[x].generation &&
+              i.species.shiny === achievementData.achievements[x].shiny
+          ).length;
+        } else if (achievementData.achievements[x].type === "Type") {
+          max = speciesData.species.filter(
+            (s) =>
+              (s.typeOne === achievementData.achievements[x].attribute ||
+                s.typeTwo === achievementData.achievements[x].attribute) &&
+              s.generation === achievementData.achievements[x].generation &&
+              s.shiny === achievementData.achievements[x].shiny
+          ).length;
+          value = instanceData.instances.filter(
+            (i) =>
+              (i.species.typeOne ===
+                achievementData.achievements[x].attribute ||
+                i.species.typeTwo ===
+                  achievementData.achievements[x].attribute) &&
+              i.species.generation ===
+                achievementData.achievements[x].generation &&
+              i.species.shiny === achievementData.achievements[x].shiny
+          ).length;
+        }
+
+        const low = 0.35 * max;
+        const high = 0.7 * max;
+        const percent = Math.round((value / max) * 100);
+        const achieved = userAchievementData.userAchievements.some(
+          (u) => u.achievementId === achievementData.achievements[x].id
+        );
+
+        full.push({
+          achievement: achievementData.achievements[x],
+          max: max,
+          value: value,
+          low: low,
+          high: high,
+          percent: percent,
+          achieved: achieved
+        });
+      }
+
+      full.sort(function (a, b) {
+        if (a.achieved > b.achieved) return 1;
+        if (b.achieved > a.achieved) return -1;
+
+        if (a.percent >= b.percent) return -1;
+        if (b.percent < a.percent) return 1;
+        return 0;
+      });
+
+      setFullAchievements(full);
+    }
+  }, [instanceData, speciesData, achievementData, userAchievementData]);
 
   const updateYield = (x: number) => {
     setTotalYield((prevTotalYield) => prevTotalYield + x);
   };
 
-  if (loading) return <Loading />;
+  if (loading || status === "loading" || countLoading) return <Loading />;
 
   return (
     <>
@@ -192,13 +173,13 @@ export default function Achievements({
         className={`min-h-screen ${time} bg-gradient-to-r from-bg-left to-bg-right text-color-text`}>
         <Sidebar page="Achievements">
           <Topbar
-            username={user.username}
-            balance={user.balance}
+            username={session.user.username}
+            balance={session.user.balance}
             totalYield={totalYield}
-            totalCards={instances.length}
+            totalCards={countData?.count!}
           />
           <main className="p-4">
-            {user?.admin && (
+            {session.user?.admin && (
               <div className="flex justify-center bg-red-500">
                 <button
                   onClick={() => setTime(time === "day" ? "night" : "day")}
@@ -207,29 +188,35 @@ export default function Achievements({
                 </button>
               </div>
             )}
-            <div className="flex justify-center pt-5">
-              <ul className="w-3/4">
-                {fullAchievements.map((a) => (
-                  <li
-                    key={a.achievement.id}
-                    className="mb-5 flex h-14 items-center justify-between border-2 border-tooltip-border p-2">
-                    <div>
-                      <b>Tier {a.achievement.tier}</b> |{" "}
-                      {a.achievement.description}
-                    </div>
-                    <div>
-                      <ProgressBar
-                        user={user}
-                        species={species}
-                        instances={instances}
-                        fullAchievement={a}
-                        updateYield={updateYield}
-                      />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            {fullAchievements ? (
+              <div className="flex justify-center pt-5">
+                <ul className="w-3/4">
+                  {fullAchievements.map((a) => (
+                    <li
+                      key={a.achievement.id}
+                      className="mb-5 flex h-14 items-center justify-between border-2 border-tooltip-border p-2">
+                      <div>
+                        <b>Tier {a.achievement.tier}</b> |{" "}
+                        {a.achievement.description}
+                      </div>
+                      <div>
+                        <ProgressBar
+                          user={session.user}
+                          species={speciesData?.species!}
+                          instances={instanceData?.instances!}
+                          fullAchievement={a}
+                          updateYield={updateYield}
+                        />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <div className="flex items-center justify-center pt-5">
+                <LoadingSpinner />
+              </div>
+            )}
           </main>
         </Sidebar>
       </div>
