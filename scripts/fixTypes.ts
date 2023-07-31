14ae54166292efac9037642881755b4301623b24
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-31 10:52:10 -0400

    Add script to fetch types and capitalize first letter

---
+++ scripts/fixTypes.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../src/server/db";
+
+// Script to populate Species collection
+const fixTypes = async () => {
+  for (let i = 1; i <= 493; i++) {
+    const pokemonResponse = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${i}`
+    );
+    const pokemonData = await pokemonResponse.json();
+    if (pokemonData.types.length === 2) {
+      const species = await prisma.species.updateMany({
+        where: { pokedexNumber: i },
+        data: {
+          typeOne:
+            pokemonData.types[0].type.name[0].toUpperCase() +
+            pokemonData.types[0].type.name.slice(1).toLowerCase(),
+          typeTwo:
+            pokemonData.types[1].type.name[0].toUpperCase() +
+            pokemonData.types[1].type.name.slice(1).toLowerCase()
+        }
+      });
+      console.log(species);
+    } else {
+      const species = await prisma.species.updateMany({
+        where: { pokedexNumber: i },
+        data: {
+          typeOne:
+            pokemonData.types[0].type.name[0].toUpperCase() +
+            pokemonData.types[0].type.name.slice(1).toLowerCase()
+        }
+      });
+      console.log(species);
+    }
+  }
+};
+
+fixTypes();
