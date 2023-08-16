d9a7b45de61d8437a6ad61e257094283ce227f83
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-16 13:14:00 -0400

    Add 4.16 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -63,6 +63,12 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">4.16 (August 16, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Caught filter to Pokédex page.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">4.15 (August 14, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
