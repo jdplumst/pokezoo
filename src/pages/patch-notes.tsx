d7c78cd78886e44db0758de6d3bfc2ef6939051d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-09-29 14:31:49 -0400

    Add 5.02 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -64,6 +64,14 @@ export default function PatchNotes() {
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">
+                5.02 (September 29, 2023)
+              </h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Unova Achievements.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">
                 5.01 (September 28, 2023)
               </h3>
               <hr className="border-black"></hr>
