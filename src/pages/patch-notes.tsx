aad55df96ee8626703780727d834e01b8af2fc29
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-10-07 01:14:27 -0400

    Add 5.04 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -67,6 +67,16 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">5.04 (October 7, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">
+                Added Wildcards! Everytime a user claims a daily or nightly
+                reward, they receive 1 wildcard in addition to the rewards they
+                already receive.
+              </p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">5.03 (October 3, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
