7ccaa58509ef8a1a4d0128a4d8162df24c299f27
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-10-09 11:52:19 -0400

    Add 5.05 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -67,6 +67,15 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">5.05 (October 9, 2023)</h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">
+                Display modal after user claims their daily and nightly rewards
+                to show the rewards that they claimed.
+              </p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">5.04 (October 7, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
