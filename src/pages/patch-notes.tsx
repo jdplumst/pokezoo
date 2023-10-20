413d9c890a8413dfbb51b6720d9d488e695758ca
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-10-19 23:59:31 -0400

    Add 5.06 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -67,6 +67,14 @@ export default function PatchNotes() {
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">
+                5.06 (October 19, 2023)
+              </h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added Region information to cards.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">5.05 (October 9, 2023)</h3>
               <hr className="border-black"></hr>
               <p className="px-4 pt-4">
