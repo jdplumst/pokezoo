392d3859f366b1cbcb12842c6424b4a4bc3252cb
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-22 12:08:26 -0500

    Add getPokedex procedure

--- src/server/api/routers/species.ts
+++ src/server/api/routers/species.ts
@@ -20,5 +20,93 @@ export const speciesRouter = router({
       }
 
       return { species: species };
+    }),
+
+  getPokedex: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        caught: z.object({ Caught: z.boolean(), Uncaught: z.boolean() }),
+        shiny: z.boolean(),
+        regions: z.array(
+          z.enum(["Kanto", "Johto", "Hoenn", "Sinnoh", "Unova"])
+        ),
+        rarities: z.array(z.enum(["Common", "Rare", "Epic", "Legendary"])),
+        types: z.array(
+          z.enum([
+            "Normal",
+            "Grass",
+            "Bug",
+            "Fire",
+            "Electric",
+            "Ground",
+            "Water",
+            "Fighting",
+            "Poison",
+            "Rock",
+            "Ice",
+            "Ghost",
+            "Psychic",
+            "Fairy",
+            "Dark",
+            "Dragon",
+            "Steel",
+            "Flying"
+          ])
+        ),
+        habitats: z.array(
+          z.enum([
+            "Grassland",
+            "Forest",
+            "WatersEdge",
+            "Sea",
+            "Cave",
+            "Mountain",
+            "RoughTerrain",
+            "Urban",
+            "Rare"
+          ])
+        )
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const limit = input.limit ?? 50;
+
+      const pokemon = await ctx.prisma.species.findMany({
+        take: limit + 1,
+        include: {
+          instances: {
+            distinct: ["speciesId"],
+            where: { userId: ctx.session.user.id }
+          }
+        },
+        where: {
+          shiny: input.shiny,
+          region: { in: input.regions },
+          rarity: { in: input.rarities },
+          OR: [
+            { typeOne: { in: input.types } },
+            { typeTwo: { in: input.types } }
+          ],
+          habitat: { in: input.habitats },
+          instances:
+            input.caught.Caught && !input.caught.Uncaught
+              ? { some: { userId: ctx.session.user.id } }
+              : !input.caught.Caught && input.caught.Uncaught
+              ? { none: { userId: ctx.session.user.id } }
+              : undefined
+        },
+        cursor: input.cursor ? { id: input.cursor } : undefined,
+        orderBy: { pokedexNumber: "asc" }
+      });
+
+      let nextCursor: typeof input.cursor | undefined = undefined;
+      if (pokemon.length > limit) {
+        const nextItem = pokemon.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      return { pokemon, nextCursor };
     })
 });
