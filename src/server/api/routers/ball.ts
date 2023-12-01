04d75a19df705bfaf5517d79a97eb1307dca00b1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-12-01 09:28:54 -0500

    Use Drizzle for getUserAchievements procedure

--- src/server/api/routers/ball.ts
+++ src/server/api/routers/ball.ts
@@ -4,7 +4,6 @@ import { ball } from "../../db/schema";
 export const ballRouter = router({
   getBalls: protectedProcedure.query(async ({ ctx }) => {
     const balls = await ctx.db.select().from(ball);
-    // const balls = await ctx.prisma.ball.findMany();
     return { balls: balls };
   })
 });
