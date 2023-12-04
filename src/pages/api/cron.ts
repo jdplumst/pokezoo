cdd2797100a2c935aa9fd752cfbefab92fb238d4
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-12-04 04:23:31 -0500

    Use Drizzle for cron

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,5 +1,8 @@
-import { prisma } from "@/src/server/db";
+import { db } from "@/src/server/db/index";
 import { NextApiRequest, NextApiResponse } from "next";
+import { user } from "@/src/server/db/schema";
+import { eq } from "drizzle-orm";
+import { MAX_BALANCE } from "@/src/constants";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { authorization } = req.headers;
@@ -12,20 +15,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "POST":
       try {
-        const users = await prisma.user.findMany();
-        for (const user of users) {
+        const users = await db.select().from(user);
+
+        for (const u of users) {
           const newBalance =
-            user.balance + user.totalYield > 1000000000
-              ? 1000000000
-              : user.balance + user.totalYield;
-          await prisma.user.update({
-            where: { id: user.id },
-            data: {
+            u.balance + u.totalYield > MAX_BALANCE
+              ? MAX_BALANCE
+              : u.balance + u.totalYield;
+
+          await db
+            .update(user)
+            .set({
               balance: newBalance,
               claimedDaily: false,
               claimedNightly: false
-            }
-          });
+            })
+            .where(eq(user.id, u.id));
         }
         return res.status(200).json({
           msg: "Successfully updated all user's dollars and reset daily and nightly claims"
