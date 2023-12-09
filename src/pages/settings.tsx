53e7b5ae757ea993b71144006c254e1215254e59
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-12-09 17:22:33 -0500

    Add Next Head to Settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -7,6 +7,7 @@ import { ZodTheme, type ZodTime } from "../zod";
 import { type z } from "zod";
 import Loading from "../components/Loading";
 import { ThemeContext } from "../components/ThemeContextProvider";
+import Head from "next/head";
 
 export default function Settings() {
   const { status } = useSession({ required: true });
@@ -47,114 +48,126 @@ export default function Settings() {
   if (status === "loading") return <Loading />;
 
   return (
-    <ThemeWrapper>
-      <Sidebar page="Settings">
-        <Topbar />
-        <main className="p-8">
-          <div className="flex justify-around">
-            <div className="flex flex-col gap-2">
-              <p className="text-3xl font-bold">Day Theme</p>
-              <div className="flex gap-5">
+    <>
+      <Head>
+        <title>PokéZoo - Settings</title>
+        <meta name="description" content="PokéZoo" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <ThemeWrapper>
+        <Sidebar page="Settings">
+          <Topbar />
+          <main className="p-8">
+            <div className="flex justify-around">
+              <div className="flex flex-col gap-2">
+                <p className="text-3xl font-bold">Day Theme</p>
+                <div className="flex gap-5">
+                  <button
+                    onClick={() => {
+                      setDayTheme("blue");
+                      handleTheme("day", "blue");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      dayTheme === "blue" ? `bg-blue-600` : `bg-blue-500`
+                    } hover:bg-blue-600 `}>
+                    Blue
+                  </button>
+                  <button
+                    onClick={() => {
+                      setDayTheme("purple");
+                      handleTheme("day", "purple");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      dayTheme === "purple" ? `bg-purple-600` : `bg-purple-500`
+                    } hover:bg-purple-600`}>
+                    Purple
+                  </button>
+                  <button
+                    onClick={() => {
+                      setDayTheme("green");
+                      handleTheme("day", "green");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      dayTheme === "green" ? `bg-green-600` : `bg-green-500`
+                    } hover:bg-green-600`}>
+                    Green
+                  </button>
+                  <button
+                    onClick={() => {
+                      setDayTheme("orange");
+                      handleTheme("day", "orange");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      dayTheme === "orange" ? `bg-orange-600` : `bg-orange-500`
+                    } hover:bg-orange-600`}>
+                    Orange
+                  </button>
+                </div>
                 <button
-                  onClick={() => {
-                    setDayTheme("blue");
-                    handleTheme("day", "blue");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    dayTheme === "blue" ? `bg-blue-600` : `bg-blue-500`
-                  } hover:bg-blue-600 `}>
-                  Blue
-                </button>
-                <button
-                  onClick={() => {
-                    setDayTheme("purple");
-                    handleTheme("day", "purple");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    dayTheme === "purple" ? `bg-purple-600` : `bg-purple-500`
-                  } hover:bg-purple-600`}>
-                  Purple
-                </button>
-                <button
-                  onClick={() => {
-                    setDayTheme("green");
-                    handleTheme("day", "green");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    dayTheme === "green" ? `bg-green-600` : `bg-green-500`
-                  } hover:bg-green-600`}>
-                  Green
-                </button>
-                <button
-                  onClick={() => {
-                    setDayTheme("orange");
-                    handleTheme("day", "orange");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    dayTheme === "orange" ? `bg-orange-600` : `bg-orange-500`
-                  } hover:bg-orange-600`}>
-                  Orange
-                </button>
-              </div>
-              <button
-                onClick={() => saveTheme("day", dayTheme)}
-                className="w-full rounded-lg border-2 border-black bg-red-btn-unfocus p-2 font-bold outline-none hover:bg-red-btn-focus">
-                Save
-              </button>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-3xl font-bold">Night Theme</p>
-              <div className="flex gap-5">
-                <button
-                  onClick={() => {
-                    setNightTheme("blue");
-                    handleTheme("night", "blue");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    nightTheme === "blue" ? `bg-blue-600` : `bg-blue-500`
-                  } hover:bg-blue-600 `}>
-                  Blue
-                </button>
-                <button
-                  onClick={() => {
-                    setNightTheme("purple");
-                    handleTheme("night", "purple");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    nightTheme === "purple" ? `bg-purple-600` : `bg-purple-500`
-                  } hover:bg-purple-600`}>
-                  Purple
-                </button>
-                <button
-                  onClick={() => {
-                    setNightTheme("green");
-                    handleTheme("night", "green");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    nightTheme === "green" ? `bg-green-600` : `bg-green-500`
-                  } hover:bg-green-600`}>
-                  Green
-                </button>
-                <button
-                  onClick={() => {
-                    setNightTheme("orange");
-                    handleTheme("night", "orange");
-                  }}
-                  className={`h-20 w-20 border-2 border-black ${
-                    nightTheme === "orange" ? `bg-orange-600` : `bg-orange-500`
-                  } hover:bg-orange-600`}>
-                  Orange
+                  onClick={() => saveTheme("day", dayTheme)}
+                  className="w-full rounded-lg border-2 border-black bg-red-btn-unfocus p-2 font-bold outline-none hover:bg-red-btn-focus">
+                  Save
                 </button>
               </div>
-              <button
-                onClick={() => saveTheme("night", nightTheme)}
-                className="w-full rounded-lg border-2 border-black bg-red-btn-unfocus p-2 font-bold outline-none hover:bg-red-btn-focus">
-                Save
-              </button>
+              <div className="flex flex-col gap-2">
+                <p className="text-3xl font-bold">Night Theme</p>
+                <div className="flex gap-5">
+                  <button
+                    onClick={() => {
+                      setNightTheme("blue");
+                      handleTheme("night", "blue");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      nightTheme === "blue" ? `bg-blue-600` : `bg-blue-500`
+                    } hover:bg-blue-600 `}>
+                    Blue
+                  </button>
+                  <button
+                    onClick={() => {
+                      setNightTheme("purple");
+                      handleTheme("night", "purple");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      nightTheme === "purple"
+                        ? `bg-purple-600`
+                        : `bg-purple-500`
+                    } hover:bg-purple-600`}>
+                    Purple
+                  </button>
+                  <button
+                    onClick={() => {
+                      setNightTheme("green");
+                      handleTheme("night", "green");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      nightTheme === "green" ? `bg-green-600` : `bg-green-500`
+                    } hover:bg-green-600`}>
+                    Green
+                  </button>
+                  <button
+                    onClick={() => {
+                      setNightTheme("orange");
+                      handleTheme("night", "orange");
+                    }}
+                    className={`h-20 w-20 border-2 border-black ${
+                      nightTheme === "orange"
+                        ? `bg-orange-600`
+                        : `bg-orange-500`
+                    } hover:bg-orange-600`}>
+                    Orange
+                  </button>
+                </div>
+                <button
+                  onClick={() => saveTheme("night", nightTheme)}
+                  className="w-full rounded-lg border-2 border-black bg-red-btn-unfocus p-2 font-bold outline-none hover:bg-red-btn-focus">
+                  Save
+                </button>
+              </div>
             </div>
-          </div>
-        </main>
-      </Sidebar>
-    </ThemeWrapper>
+          </main>
+        </Sidebar>
+      </ThemeWrapper>
+    </>
   );
 }
