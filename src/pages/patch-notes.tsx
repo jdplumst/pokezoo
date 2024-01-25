62b0c8b976661109c8ce4b5e371490ab7d82e489
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-01-25 13:41:34 -0500

    Add 6.02 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -28,6 +28,16 @@ export default function PatchNotes() {
             <hr className="border-black pb-4"></hr>
             <section className="pb-4">
               <h3 className="p-4 text-4xl font-bold">
+                6.02 (January 25, 2023)
+              </h3>
+              <hr className="border-black"></hr>
+              <p className="px-4 pt-4">Added a new rarity: Mega!</p>
+              <p className="px-4">Added Kanto Megas.</p>
+              <p className="px-4">Added Cherish Ball.</p>
+            </section>
+            <hr className="border-black pb-4"></hr>
+            <section className="pb-4">
+              <h3 className="p-4 text-4xl font-bold">
                 6.01 (January 24, 2023)
               </h3>
               <hr className="border-black"></hr>
