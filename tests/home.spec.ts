399dcb6dbac21cfb65283b88c21db86d10bb3504
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-01-28 23:37:57 -0500

    Set up Playwright

---
+++ tests/home.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, Page } from "@playwright/test";
+
+let page: Page;
+
+test.beforeAll("go to home page", async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+});
+
+test("has title", async () => {
+  await expect(page).toHaveTitle("PokéZoo");
+});
+
+test("has login buttons", async () => {
+  await expect(page.getByText("Sign In With GitHub")).toBeVisible();
+  await expect(page.getByText("Sign In With Twitch")).toBeVisible();
+  await expect(page.getByText("Sign In With Google")).toBeVisible();
+});
