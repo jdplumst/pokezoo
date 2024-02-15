190d50872017a442412ea032a9df22b8b0d7231e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-02-15 12:07:23 -0500

    remove parallelism from playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,14 +11,14 @@ require("dotenv").config();
  */
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
+  /* Do not run tests in files in parallel */
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry */
   retries: process.env.CI ? 2 : 2,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests. */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
