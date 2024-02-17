1dafc37ca19e1009ddd1c715d1c7fb6797356450
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-02-17 14:02:30 -0500

    modify instance limit for oval charm

--- src/utils/withinInstanceLimit.ts
+++ src/utils/withinInstanceLimit.ts
@@ -1,3 +1,4 @@
-export function withinInstanceLimit(instanceCount: number) {
-  return instanceCount < 2000;
+export function withinInstanceLimit(instanceCount: number, ovalCharm: boolean) {
+  const limit = ovalCharm ? 3000 : 2000;
+  return instanceCount < limit;
 }
