4c62e605c13286fb8322bfe30d4dbf738767ab36
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-02-27 17:17:08 -0500

    add env var for resetting test accounts

--- src/env.ts
+++ src/env.ts
@@ -28,7 +28,8 @@ export const env = createEnv({
     TEST_VALUE1: z.string(),
     TEST_UNAME1: z.string(),
     TEST_VALUE2: z.string(),
-    TEST_UNAME2: z.string()
+    TEST_UNAME2: z.string(),
+    TEST_RESET: z.string().url()
   },
   client: {},
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
@@ -50,7 +51,8 @@ export const env = createEnv({
     TEST_VALUE1: process.env.TEST_VALUE1,
     TEST_UNAME1: process.env.TEST_UNAME1,
     TEST_VALUE2: process.env.TEST_VALUE2,
-    TEST_UNAME2: process.env.TEST_UNAME2
+    TEST_UNAME2: process.env.TEST_UNAME2,
+    TEST_RESET: process.env.TEST_RESET
   },
   emptyStringAsUndefined: true,
   skipValidation: process.env.SKIP_VALIDATION === "true"
