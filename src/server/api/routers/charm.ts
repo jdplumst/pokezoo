aee25afae75e9953839a9bab3fda4dd769f75588
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-03-16 11:51:44 -0400

    Add getUserCharms procedure

--- src/server/api/routers/charm.ts
+++ src/server/api/routers/charm.ts
@@ -76,5 +76,31 @@ export const charmRouter = router({
       });
 
       return { message: "Charm bought successfully" };
-    })
+    }),
+
+  getUserCharms: protectedProcedure.query(async ({ ctx }) => {
+    const currUser = (
+      await ctx.db
+        .select({ balance: profiles.balance })
+        .from(profiles)
+        .where(eq(profiles.userId, ctx.session.user.id))
+    )[0];
+
+    if (!currUser) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Not authorized to make this request"
+      });
+    }
+
+    try {
+      const charmsData = await ctx.db.select().from(userCharms).leftJoin(charms, eq(userCharms.charmId, charms.id))
+      return { charmsData }
+    } catch (err) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Something went wrong. Try again later."
+      })
+    }
+  })
 });
