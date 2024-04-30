f51c60d8796eaf3f2c66fe1397c9855bd29936a2
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-04-29 20:01:29 -0400

    Set project type to Module

--- next.config.js
+++ next.config.js
@@ -8,8 +8,9 @@ const nextConfig = {
   transpilePackages: ["geist"],
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
+import withBundleAnalyzer from "@next/bundle-analyzer";
+const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+export default bundleAnalyzer(nextConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -70,3 +70,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
