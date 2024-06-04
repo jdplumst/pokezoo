77b59b7c84a9f4bcb8e33916e9db31ee2b1bce3c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-06-04 18:18:47 -0400

    Add purchase dive balls test

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -101,3 +101,18 @@ test("purchase dusk balls", async ({ browser }) => {
 
   await closePage(page, context);
 });
+
+test("purchase dive balls", async ({ browser }) => {
+  const [page, context] = await login(browser, 1);
+  await page.goto("/shop");
+  await expect(page.getByText(env.TEST_UNAME1)).toBeVisible();
+
+  // Purchase 10 times
+  for (let i = 0; i < 10; i++) {
+    await page.getByTestId("Dusk-button").click();
+    await expect(page.getByText(/Habitat: (Waters-Edge|Sea)/)).toBeVisible();
+    await page.getByTestId("confirm-ball-button").click();
+  }
+
+  await closePage(page, context);
+});
