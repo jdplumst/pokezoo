b90795687c5c3d18ff3b1acd91c250b750915553
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-10-19 12:05:33 -0400

    Add 8.01 patch notes

--- src/pages/patch-notes.tsx
+++ src/pages/patch-notes.tsx
@@ -37,6 +37,12 @@ export default function PatchNotes() {
           <main className="px-8">
             <h1 className="p-4 text-7xl font-bold">Patch Notes</h1>
             <Patch
+              patch="8.01"
+              date="October 19, 2024"
+            >
+              <Note note="Added Galarian regional forms." />
+            </Patch>
+            <Patch
               patch="8.00"
               date="October 18, 2024"
             >
