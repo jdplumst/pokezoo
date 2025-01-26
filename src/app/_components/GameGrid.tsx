c08f78e2b35df70ee4261e369bc55f74876b3ec5
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-25 23:35:47 -0500

    Show loading spinner when initially loading for fetching game page pokemon

--- src/app/_components/GameGrid.tsx
+++ src/app/_components/GameGrid.tsx
@@ -340,6 +340,7 @@ export default function GameGrid() {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-10">
+        {pokemon.isInitialLoading && <LoadingSpinner />}
         <div
           className={`grid ${open ? `grid-cols-1 lg:grid-cols-2 xl:grid-cols-3` : `grid-cols-1 lg:grid-cols-2 xl:grid-cols-3`} gap-10`}
         >
