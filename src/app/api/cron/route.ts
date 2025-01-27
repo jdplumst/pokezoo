d31b3631a68cf92a5e872f6d42270b11442d434d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-27 01:42:12 -0500

    Move cron to app router

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,123 @@
+import { db } from "@/src/server/db/index";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import {
+  profiles,
+  quests,
+  type selectProfileSchema,
+  userQuests,
+} from "@/src/server/db/schema";
+import { eq, sql } from "drizzle-orm";
+import { MAX_BALANCE } from "@/src/constants";
+import { env } from "@/src/env";
+import { type z } from "zod";
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { authorization } = req.headers;
+  if (!authorization || authorization.split(" ")[1] !== env.CRON_TOKEN) {
+    throw new Error("Not authorized to make this request.");
+  }
+  try {
+    const date = new Date();
+    const dayOfWeek = date.getDay();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+
+    // Set claimedEvent field
+    let completedEvent = null;
+
+    if (
+      (day === 25 && month === 12) ||
+      (day === 1 && month === 1) ||
+      (day === 5 && month === 4)
+    ) {
+      completedEvent = false;
+    } else if ((day === 8 && month === 1) || (day === 13 && month === 4)) {
+      completedEvent = true;
+    }
+
+    // Set user quests
+    await db.execute(
+      sql`DELETE FROM "pokezoo_userQuest"
+              WHERE "pokezoo_userQuest"."questId" IN (
+              SELECT id
+              FROM "pokezoo_quest"
+              WHERE "pokezoo_quest"."type" = 1)`,
+    );
+
+    if (dayOfWeek === 0) {
+      await db.execute(
+        sql`DELETE FROM "pokezoo_userQuest"
+              WHERE "pokezoo_userQuest"."questId" IN (
+              SELECT id
+              FROM "pokezoo_quest"
+              WHERE "pokezoo_quest"."type" = 2)`,
+      );
+    }
+
+    if (day === 1) {
+      await db.execute(
+        sql`DELETE FROM "pokezoo_userQuest"
+              WHERE "pokezoo_userQuest"."questId" IN (
+              SELECT id
+              FROM "pokezoo_quest"
+              WHERE "pokezoo_quest"."type" = 3)`,
+      );
+    }
+
+    // Update balance from yield
+    const profilesData = await db.select().from(profiles);
+
+    for (const profile of profilesData) {
+      const newBalance =
+        profile.balance + profile.totalYield > MAX_BALANCE
+          ? MAX_BALANCE
+          : profile.balance + profile.totalYield;
+
+      await setQuests(profile, 1);
+      if (dayOfWeek === 0) {
+        await setQuests(profile, 2);
+      }
+      if (day === 1) {
+        await setQuests(profile, 3);
+      }
+
+      await db
+        .update(profiles)
+        .set({
+          balance: newBalance,
+          claimedDaily: false,
+          claimedNightly: false,
+          claimedEvent: completedEvent ?? profile.claimedEvent,
+        })
+        .where(eq(profiles.id, profile.id));
+    }
+    return res.status(200).json({
+      msg: "Successfully updated all user's dollars and reset daily and nightly claims",
+    });
+  } catch (_) {
+    throw new Error(
+      "User's dollars and daily and nightly claims not updated successfully",
+    );
+  }
+}
+
+async function setQuests(
+  currProfile: z.infer<typeof selectProfileSchema>,
+  questType: number,
+) {
+  const randomQuests = await db
+    .select()
+    .from(quests)
+    .where(eq(quests.typeId, questType))
+    .orderBy(sql`RANDOM()`)
+    .limit(5);
+
+  for (const currQuest of randomQuests) {
+    await db.insert(userQuests).values({
+      userId: currProfile.userId,
+      questId: currQuest.id,
+      count: 0,
+      claimed: false,
+    });
+  }
+}
