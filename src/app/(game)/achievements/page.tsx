53daa3a9748f7768fa881aecdfe1aab1543704bc
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-27 11:23:52 -0500

    Move achievments page to app router

---
+++ src/app/(game)/achievements/page.tsx
@@ -0,0 +1,70 @@
+import { Button } from "@/src/components/ui/button";
+import { Separator } from "@/src/components/ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/src/components/ui/table";
+import { getAchievements } from "@/src/server/actions/achievements";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PokéZoo - Achievements",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default async function Achievements() {
+  const achievements = await getAchievements();
+
+  return (
+    <div className="px-8 pb-8">
+      <h1 className="py-4 text-5xl font-bold">Achievements</h1>
+      <Separator className="mb-4" />
+      <Table>
+        <TableCaption hidden={true}>
+          A list of your achievement progress.
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Tier</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Reward</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {achievements.map((a) => (
+            <TableRow key={a.achievement.id}>
+              <TableCell className="font-medium">
+                {a.achievement.tier}
+              </TableCell>
+              <TableCell>{a.achievement.description}</TableCell>
+              <TableCell>P{a.achievement.yield.toLocaleString()}</TableCell>
+              <TableCell>
+                {a.achievement.claimed ? (
+                  "Claimed"
+                ) : a.value === a.max ? (
+                  <form
+                    action={async () => {
+                      "use server";
+                    }}
+                  >
+                    <Button>Claim</Button>
+                  </form>
+                ) : (
+                  a.value + " / " + a.max + " (" + a.percent + "%)"
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
---
+++ src/server/actions/achievements.ts
@@ -0,0 +1,170 @@
+import "server-only";
+import { auth } from "../auth";
+import { redirect } from "next/navigation";
+import { db } from "../db";
+import {
+  achievements,
+  achievementTypes,
+  attributes,
+  habitats,
+  instances,
+  rarities,
+  regions,
+  species,
+  types,
+  userAchievements,
+} from "../db/schema";
+import { and, eq } from "drizzle-orm";
+import { alias } from "drizzle-orm/pg-core";
+
+export async function getAchievements() {
+  const session = await auth();
+  if (!session) {
+    redirect("/");
+  }
+
+  const typeOne = alias(types, "typeOne");
+  const typeTwo = alias(types, "typeTwo");
+
+  const achievementsData = await db
+    .select({
+      id: achievements.id,
+      description: achievements.description,
+      tier: achievements.tier,
+      yield: achievements.yield,
+      type: achievementTypes.name,
+      attribute: attributes.name,
+      region: regions.name,
+      shiny: achievements.shiny,
+      generation: achievements.generation,
+      claimed: userAchievements.id,
+    })
+    .from(achievements)
+    .leftJoin(
+      userAchievements,
+      and(
+        eq(userAchievements.achievementId, achievements.id),
+        eq(userAchievements.userId, session.user.id),
+      ),
+    )
+    .innerJoin(achievementTypes, eq(achievements.typeId, achievementTypes.id))
+    .innerJoin(attributes, eq(achievements.attributeId, attributes.id))
+    .innerJoin(regions, eq(achievements.regionId, regions.id));
+
+  const speciesData = await db
+    .select({
+      speciesId: species.id,
+      instanceId: instances.id,
+      region: regions.name,
+      rarity: rarities.name,
+      habitat: habitats.name,
+      shiny: species.shiny,
+      typeOne: typeOne.name,
+      typeTwo: typeTwo.name,
+    })
+    .from(species)
+    .leftJoin(
+      instances,
+      and(
+        eq(instances.speciesId, species.id),
+        eq(instances.userId, session.user.id),
+      ),
+    )
+    .innerJoin(regions, eq(regions.id, species.regionId))
+    .innerJoin(rarities, eq(rarities.id, species.rarityId))
+    .innerJoin(habitats, eq(habitats.id, species.habitatId))
+    .innerJoin(typeOne, eq(typeOne.id, species.typeOneId))
+    .leftJoin(typeTwo, eq(typeTwo.id, species.typeTwoId));
+
+  const fullAchievements = [];
+
+  for (const a of achievementsData) {
+    let max = 0;
+    let value = 0;
+
+    if (a.type === "All") {
+      max = speciesData.filter(
+        (s) =>
+          s.region === a.region &&
+          s.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(s.rarity),
+      ).length;
+      value = speciesData.filter(
+        (i) =>
+          i.instanceId &&
+          i.region === a.region &&
+          i.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(i.rarity),
+      ).length;
+    } else if (a.type === "Rarity") {
+      max = speciesData.filter(
+        (s) =>
+          s.rarity === a.attribute &&
+          (a.generation ? s.region === a.region : s.region) &&
+          s.shiny === a.shiny,
+      ).length;
+      value = speciesData.filter(
+        (i) =>
+          i.instanceId &&
+          i.rarity === a.attribute &&
+          (a.generation ? i.region === a.region : i.region) &&
+          i.shiny === a.shiny,
+      ).length;
+    } else if (a.type === "Habitat") {
+      max = speciesData.filter(
+        (s) =>
+          s.habitat === a.attribute &&
+          s.region === a.region &&
+          s.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(s.rarity),
+      ).length;
+      value = speciesData.filter(
+        (i) =>
+          i.instanceId &&
+          i.habitat === a.attribute &&
+          i.region === a.region &&
+          i.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(i.rarity),
+      ).length;
+    } else if (a.type === "Type") {
+      max = speciesData.filter(
+        (s) =>
+          (s.typeOne === a.attribute || s.typeTwo === a.attribute) &&
+          s.region === a.region &&
+          s.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(s.rarity),
+      ).length;
+      value = speciesData.filter(
+        (i) =>
+          i.instanceId &&
+          (i.typeOne === a.attribute || i.typeTwo === a.attribute) &&
+          i.region === a.region &&
+          i.shiny === a.shiny &&
+          ["Common", "Rare", "Epic", "Legendary"].includes(i.rarity),
+      ).length;
+    }
+
+    const low = 0.35 * max;
+    const high = 0.7 * max;
+    const percent = Math.round((value / max) * 100);
+
+    fullAchievements.push({
+      achievement: a,
+      max: max,
+      value: value,
+      low: low,
+      high: high,
+      percent: percent,
+    });
+  }
+
+  fullAchievements.sort(function (a, b) {
+    if (a.achievement.claimed == b.achievement.claimed) {
+      return b.percent - a.percent;
+    }
+    return a.achievement.claimed ? 1 : -1;
+  });
+
+  console.log(fullAchievements);
+  return fullAchievements;
+}
