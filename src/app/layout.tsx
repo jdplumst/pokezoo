3f77e9b10e20691756e62389a78909ae27dce3d3
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-03-27 18:26:28 -0400

    Add react scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { Toaster } from "~/components/ui/sonner";
 import { TRPCReactProvider } from "~/trpc/react";
 import { getTheme, getTime } from "~/server/db/queries/cookies";
+import { env } from "~/env";
 
 export default async function RootLayout({
   children,
@@ -15,6 +16,14 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${GeistSans.className} ${theme} ${mode}`}>
+      {env.NODE_ENV === "development" && (
+        <head>
+          <script
+            src="https://unpkg.com/react-scan/dist/auto.global.js"
+            async
+          />
+        </head>
+      )}
       <body>
         <Toaster />
         <TRPCReactProvider>{children}</TRPCReactProvider>
