2be5ac3cc953568bbd24e24920c03c67c23cee64
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-04-28 18:18:20 -0400

    Add 9.06 patch notes

--- src/app/(game)/patch-notes/page.tsx
+++ src/app/(game)/patch-notes/page.tsx
@@ -16,6 +16,9 @@ export default async function PatchNotes() {
   return (
     <div className="px-8">
       <h1 className="py-4 text-5xl font-bold">Patch Notes</h1>
+      <Patch patch="9.06" date="April 28, 2025">
+        <Note note="Fixed wrong pokémon showing up in user's storage." />
+      </Patch>
       <Patch patch="9.05" date="April 27, 2025">
         <Note note="Fixed pokémon being able to be moved into full boxes." />
       </Patch>
