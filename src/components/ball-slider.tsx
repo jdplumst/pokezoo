6e7e94b25ea88425a9a7b7299c0ab1a1c94ac763
jdplumst <jeremiah.plumstead@gmail.com>
2026-01-03 19:49:12 -0500

    Fix ball purchase modal height

--- src/components/ball-slider.tsx
+++ src/components/ball-slider.tsx
@@ -158,7 +158,7 @@ export default function BallSlider(props: {
             <DialogDescription>
               Here is all the Pokémon you have obtained.
             </DialogDescription>
-            <div className="flex h-80 flex-col gap-4">
+            <div className="flex flex-col gap-4">
               {data.purchasedSpecies.map((s, idx) => (
                 <MiniPokemonCard
                   key={idx}
